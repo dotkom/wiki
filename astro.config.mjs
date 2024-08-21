20573731924a204d1381f6ca0ed510ce2dab7ddb
Henrik Hørlück Berg <36937807+henrikhorluck@users.noreply.github.com>
2024-08-22 01:28:39 +0200

    Add astro config

---
+++ astro.config.mjs
@@ -0,0 +1,57 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+import fs from "node:fs";
+
+const roots = fs.readdirSync("src/content/docs").filter(v => [".DS_Store", "index.md"].indexOf(v) === -1)
+
+// https://astro.build/config
+export default defineConfig({
+    site: "https://wiki.online.ntnu.no",
+    integrations: [
+        starlight({
+            title: 'Online Wiki',
+            defaultLocale: "root",
+            locales: {
+                root: {
+                    label: "Norsk",
+                    lang: "no",
+                },
+                en: {
+                    label: "English",
+                },
+            },
+            logo: {
+                light: "./src/assets/img/online_bla_o.svg",
+                dark:"./src/assets/img/online_hvit_o.svg",
+            },
+            lastUpdated: true,
+            customCss: [
+                "./src/styles/custom.css",
+            ],
+            editLink: {baseUrl: 'https://github.com/dotkom/wiki/edit/main/'},
+            
+			social: {
+                facebook: "https://facebook.com/LinjeforeningenOnline",
+                instagram: "https://www.instagram.com/online_ntnu/",
+                slack: "https://onlinentnu.slack.com/",
+                github: 'https://github.com/dotkom/wiki',
+                discord: "https://discordapp.com/invite/2XB9egU",
+                
+			},
+            sidebar: [
+                {
+                    label: 'Online',
+                    link: "/",
+                },
+                ...roots.map(root => {
+                    return {
+                        label: root,
+                        collapsed: true,
+                        autogenerate: { directory: root },
+        }
+})
+			],
+		}),
+	],
+});
