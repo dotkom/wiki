4481d1a480fbef2e259c18a9b7c67e654d779c66
Henrik Hørlück Berg <36937807+henrikhorluck@users.noreply.github.com>
2024-08-22 13:40:33 +0200

    Add favicon and plausible

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,56 +1,66 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 import fs from "node:fs";
 
-const roots = fs.readdirSync("src/content/docs").filter(v => [".DS_Store", "index.md"].indexOf(v) === -1)
+const roots = fs
+	.readdirSync("src/content/docs")
+	.filter((v) => [".DS_Store", "index.md"].indexOf(v) === -1);
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://wiki.online.ntnu.no",
-    integrations: [
-        starlight({
-            title: 'Online Wiki',
-            defaultLocale: "root",
-            locales: {
-                root: {
-                    label: "Norsk",
-                    lang: "no",
-                },
-                en: {
-                    label: "English",
-                },
-            },
-            logo: {
-                light: "./src/assets/img/online_bla_o.svg",
-                dark:"./src/assets/img/online_hvit_o.svg",
-            },
-            lastUpdated: true,
-            customCss: [
-                "./src/styles/custom.css",
-            ],
-            editLink: {baseUrl: 'https://github.com/dotkom/wiki/edit/main/'},
-            
-			social: {
-                facebook: "https://facebook.com/LinjeforeningenOnline",
-                instagram: "https://www.instagram.com/online_ntnu/",
-                slack: "https://onlinentnu.slack.com/",
-                github: 'https://github.com/dotkom/wiki',
-                discord: "https://discordapp.com/invite/2XB9egU",
-                
+	site: "https://wiki.online.ntnu.no",
+	integrations: [
+		starlight({
+            title: "Online Wiki",
+            favicon: "/images/online_bla_o.svg",
+			defaultLocale: "root",
+			locales: {
+				root: {
+					label: "Norsk",
+					lang: "no",
+				},
+				en: {
+					label: "English",
+				},
 			},
-            sidebar: [
+			logo: {
+				light: "./src/assets/img/online_bla_o.svg",
+				dark: "./src/assets/img/online_hvit_o.svg",
+			},
+			lastUpdated: true,
+			customCss: ["./src/styles/custom.css"],
+			editLink: { baseUrl: "https://github.com/dotkom/wiki/edit/main/" },
+
+			social: {
+				facebook: "https://facebook.com/LinjeforeningenOnline",
+				instagram: "https://www.instagram.com/online_ntnu/",
+				slack: "https://onlinentnu.slack.com/",
+				github: "https://github.com/dotkom/wiki",
+				discord: "https://discordapp.com/invite/2XB9egU",
+			},
+            head: [
                 {
-                    label: 'Online',
-                    link: "/",
-                },
-                ...roots.map(root => {
-                    return {
-                        label: root,
-                        collapsed: true,
-                        autogenerate: { directory: root },
-        }
-})
+                    tag: "script",
+                    attrs: {
+                        defer: true,
+                        "data-domain": "wiki.online.ntnu.no",
+                        src: "https://plausible.io/js/script.outbound-links.file-downloads.js",
+                    }
+                }
+            ],
+			sidebar: [
+				{
+					label: "Online",
+					link: "/",
+				},
+				...roots.map((root) => {
+					return {
+						label: root,
+						collapsed: true,
+						autogenerate: { directory: root },
+					};
+				}),
 			],
 		}),
 	],
