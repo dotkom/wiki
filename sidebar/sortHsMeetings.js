dc994d38d1d888434a351f33d4084fb0ea0fff3c
brage-andreas <bragerino+github@gmail.com>
2025-04-11 20:07:34 +0200

    feat: update sidebar to sort HS meetings

---
+++ sidebar/sortHsMeetings.js
@@ -0,0 +1,55 @@
+import { getFrontmatter } from "./utils";
+
+const JULY = 6;
+const FIFTEENTH = 15;
+
+export const sortHsMeetings = (files) => {
+	const groups = files.reduce(
+		(acc, file) => {
+			const rawDate = getFrontmatter(file.content, "date");
+			if (!rawDate) {
+				acc.other.push(file);
+			} else {
+				const date = new Date(rawDate);
+				const cutoff = new Date(date);
+				cutoff.setMonth(JULY, FIFTEENTH);
+
+				if (date > cutoff) {
+					acc.autumn.push(file);
+				} else {
+					acc.spring.push(file);
+				}
+			}
+			return acc;
+		},
+		{ autumn: [], spring: [], other: [] }
+	);
+
+	const groupedFiles = [];
+
+	if (groups.autumn.length > 0) {
+		groupedFiles.push({
+			label: "Høst",
+			items: groups.autumn,
+			collapsed: false,
+		});
+	}
+
+	if (groups.spring.length > 0) {
+		groupedFiles.push({
+			label: "Vår",
+			items: groups.spring,
+			collapsed: false,
+		});
+	}
+
+	if (groups.other.length > 0) {
+		groupedFiles.push({
+			label: "Andre",
+			items: groups.other,
+			collapsed: false,
+		});
+	}
+
+	return groupedFiles;
+};
